3ab756a2b26c6a350ce3f68268e7b4395a928d96
mac <mac@macs-Mac-mini.local>
2025-03-24 01:24:07 -0700

    update

--- local_llms/__init__.py
+++ local_llms/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 """Local LLMs - A library to manage local language models."""
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 COMMAND_DIRS = [
     "/usr/local/bin",
